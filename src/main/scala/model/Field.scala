f64b58fd1ba777fa524f350ce2bb69a3d88ec6df
Markus Heilig <markusheilig@gmx.net>
2016-10-13 20:44:58 +0200

    changed function call order

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -6,10 +6,10 @@ case class Field(x: Int, y: Int) {
   def getLeftNeighbour = Field(x-1, y)
   def getUpperNeighbour = Field(x, y-1)
   def getLowerNeighbour = Field(x, y+1)
-  def getUpperRightNeighbour = getRightNeighbour.getUpperNeighbour
-  def getLowerRightNeighbour = getRightNeighbour.getLowerNeighbour
-  def getUpperLeftNeighbour = getLeftNeighbour.getUpperNeighbour
-  def getLowerLeftNeighbour = getLeftNeighbour.getLowerNeighbour
+  def getUpperRightNeighbour = getUpperNeighbour.getRightNeighbour
+  def getLowerRightNeighbour = getLowerNeighbour.getRightNeighbour
+  def getUpperLeftNeighbour = getUpperNeighbour.getLeftNeighbour
+  def getLowerLeftNeighbour = getLowerNeighbour.getLeftNeighbour
 
   // create a (x, y) tuple from this
   def unary_~ = Field.unapply(this).get
