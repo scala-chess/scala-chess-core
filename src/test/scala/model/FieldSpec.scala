defce34d78996168b628597b1fc008d73d7db7d0
Markus Heilig <markusheilig@gmx.net>
2016-10-15 10:30:36 +0200

    replaced tuple check with Field check

--- src/test/scala/model/FieldSpec.scala
+++ src/test/scala/model/FieldSpec.scala
@@ -19,35 +19,35 @@ class FieldSpec extends Specification { def is = s2"""
                             """
 
   def right = {
-    ~Field(0, 0).getRightNeighbour must_==(1, 0)
+    Field(0, 0).getRightNeighbour must_==Field(1, 0)
   }
 
   def left = {
-    ~Field(2, 2).getLeftNeighbour must_==(1, 2)
+    Field(2, 2).getLeftNeighbour must_==Field(1, 2)
   }
 
   def upper = {
-    ~Field(3, 5).getUpperNeighbour must_==(3, 4)
+    Field(3, 5).getUpperNeighbour must_==Field(3, 4)
   }
 
   def lower = {
-    ~Field(4, 2).getLowerNeighbour must_==(4, 3)
+    Field(4, 2).getLowerNeighbour must_==Field(4, 3)
   }
 
   def upperRight = {
-    ~Field(3, 3).getUpperRightNeighbour must_==(4, 2)
+    Field(3, 3).getUpperRightNeighbour must_==Field(4, 2)
   }
 
   def upperLeft = {
-    ~Field(3, 3).getUpperLeftNeighbour must_==(2, 2)
+    Field(3, 3).getUpperLeftNeighbour must_==Field(2, 2)
   }
 
   def lowerRight = {
-    ~Field(3, 3).getLowerRightNeighbour must_==(4, 4)
+    Field(3, 3).getLowerRightNeighbour must_==Field(4, 4)
   }
 
   def lowerLeft = {
-    ~Field(3, 3).getLowerLeftNeighbour must_==(2, 4)
+    Field(3, 3).getLowerLeftNeighbour must_==Field(2, 4)
   }
 
 }
