179524c615a5195196dc48df515cfa220f4bd40a
Andreas Reinhardt <andreasreinhardt7@gmail.com>
2016-10-22 21:41:15 +0200

    Move line function to super class (Piece)

--- src/main/scala/model/Piece.scala
+++ src/main/scala/model/Piece.scala
@@ -1,10 +1,23 @@
 package model
 
 abstract class Piece(val color: Color.Value) {
+  type Direction = ((Int,Int)) => (Int,Int)
   override def toString: String = s"$color-${getClass.getSimpleName}"
   def isBlack = color == Color.Black
   def isWhite = !isBlack
   def isAlly(that: Piece) = this.color == that.color
   def isEnemy(that: Piece) = !isAlly(that)
   def getMoves(field: (Int, Int), board: Board): Iterable[Action]
+
+  def line(dir: Direction, board: Board, pos: (Int,Int), depth: Int): List[(Int,Int)] = {
+    val target = dir(pos) 
+    if(depth > 8){
+      List()
+    } else {
+      board.get(target) match {
+        case None => target :: line(dir, board, target, depth + 1)
+        case Some(piece) => List(target)
+      }
+    }    
+  }
 }
--- src/main/scala/model/pieces/Rook.scala
+++ src/main/scala/model/pieces/Rook.scala
@@ -4,9 +4,8 @@ import model.TupleUtils._
 import model._
 
 case class Rook(c: Color.Value) extends Piece(c) {
-  type Direction = ((Int,Int)) => (Int,Int)
   override def getMoves(field: (Int, Int), board: Board): Iterable[Action] = 
-    Seq[Direction](
+    Seq(
       (t:(Int,Int)) => t.right,
       (t:(Int,Int)) => t.left,
       (t:(Int,Int)) => t.down,
@@ -17,22 +16,5 @@ case class Rook(c: Color.Value) extends Piece(c) {
       target => board.get(target) map {p => !isAlly(p)} getOrElse true 
     } map { 
       target => Move(field, target)
-    }
-
-
-  def line(dir: Direction, board: Board, pos: (Int,Int), depth: Int): List[(Int,Int)] = {
-    val target = dir(pos) 
-    if(depth > 8){
-      List()
-    } else {
-      board.get(target) match {
-        case None => target :: line(dir, board, target, depth + 1)
-        case Some(piece) => List(target)
-      }
-    }    
-  }
-
-  def right(t:(Int,Int)):(Int,Int) = {
-    t.right
-  }
+    }  
 }
