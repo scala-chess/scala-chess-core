0db810c125be0162829861a8eae403bd78db48c5
Markus Heilig <markusheilig@gmx.net>
2016-10-25 09:03:57 +0200

    refactoring

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -1,38 +1,38 @@
 package model
 
-class Board {
-  var matrix: Vector[Vector[Option[Piece]]] = Board.defaultChessMatrix
+import model.pieces.King
 
-  def get(pos: (Int, Int)): Option[Piece] = {
-    if (!inMatrix(pos)) {
-      None
-    } else {
-      matrix(pos._1)(pos._2)
+case class Board(matrix: Vector[Vector[Option[Piece]]] = Board.defaultChessMatrix) {
+  require(matrix.nonEmpty)
+  require(matrix.head.nonEmpty)
+
+  def get(pos: (Int, Int)): Option[Piece] =
+    matrix.lift(pos._1) flatMap { vec => vec.lift(pos._2).flatten }
+
+  def getAll: Iterable[((Int, Int), Piece)] = {
+    matrix.zipWithIndex flatMap {
+      case (vector, indexX) =>
+        vector.zipWithIndex map {
+          case (Some(piece), indexY) => ((indexX, indexY), piece)
+        }
     }
   }
 
-  //def getAll(): Iterable[Piece] =
-  //    matrix.flatten or something
-
-  def set(pos: (Int, Int), piece: Option[Piece]): Board =
-    if (inMatrix(pos)) {
-      val inner = this.matrix(pos._1)
-      val innerUpdated = inner.updated(pos._2, piece)
-      val matrix = this.matrix.updated(pos._1, innerUpdated)
-      val newBoard = new Board()
-      newBoard.matrix = matrix
-      newBoard
-    } else {
-      this
-    }
-
-  def inMatrix(pos: (Int, Int)): Boolean =
-    matrix.indices.contains(pos._1) && matrix(0).indices.contains(pos._2)
+  def set(pos: (Int, Int), piece: Option[Piece]): Board = {
+    matrix.lift(pos._1) flatMap {
+      _.lift(pos._2)
+    } map {
+      _ => Board(matrix.updated(pos._1, matrix(pos._1).updated(pos._2, piece)))
+    } getOrElse this
+  }
 }
 
 object Board {
-  val defaultMatrixSize = 8
 
   // 8x8
+  val defaultMatrixSize = 8
+
   def defaultChessMatrix = Vector.fill(defaultMatrixSize)(Vector.fill(defaultMatrixSize)(None))
+
+  def kingGamefield = Vector(Vector(Some(King(Color.Black)), None, Some(King(Color.White))))
 }
--- src/test/scala/model/GamefieldSpec.scala
+++ src/test/scala/model/GamefieldSpec.scala
@@ -4,45 +4,27 @@ import model.pieces.King
 import org.specs2.Specification
 
 
-class GamefieldSpec extends Specification { def is = s2"""
+class BoardSpec extends Specification { def is = s2"""
 
-  A Gamefield should
+  A Board should
     1. be easy to create            $create
-    2. check indices                $indices
     3. get information about pieces $getPiece
-    4. tell you if there is a piece $hasPiece
-    5. execute valid moves          $executeValidMoves
-    6. do not execute invalid moves $doNotExecuteInvalidMoves
+    5. set a piece                  $setPiece
                           """
 
   val create = {
-    Gamefield()
+    Board()
     true must_==true
   }
 
-  val indices = {
-    val gamefield = Gamefield()
-    gamefield.validIndices((0,0)) must_==true
-    gamefield.validIndices((7,7)) must_==true
-    gamefield.validIndices((8,0)) must_==false
-    gamefield.validIndices((0,8)) must_==false
-    gamefield.validIndices((8,8)) must_==false
-  }
-
   val getPiece = {
-    val kingGamefield = Gamefield(grid=Gamefield.kingGamefield)
-    kingGamefield.getPiece((0, 0)) must_==Some(King(Color.Black))
-    kingGamefield.getPiece((0, 1)) must_==None
-    kingGamefield.getPiece((0, 2)) must_==Some(King(Color.White))
+    val board = Gamefield(matrix=Board.kingGamefield)
+    board.get((0, 0)) must_==Some(King(Color.Black))
+    board.get((0, 1)) must_==None
+    board.get((0, 2)) must_==Some(King(Color.White))
 
   }
 
-  val hasPiece = {
-    val kingGamefield = Gamefield(grid=Gamefield.kingGamefield)
-    kingGamefield.hasPiece((0, 0)) must_==true
-    kingGamefield.hasPiece((0, 1)) must_==false
-    kingGamefield.hasPiece((0, 2)) must_==true
-  }
 
   val executeValidMoves = {
     // wrong! -> only execute move if there is a piece and the piece's moves contains this move
