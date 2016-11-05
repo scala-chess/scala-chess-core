058cde4df87a266f450f2988b00cd3aa3ac0629b
Markus Heilig <markusheilig@gmx.net>
2016-11-05 11:22:10 +0100

    create ControllerActor

--- src/main/scala/ControllerActor.scala
+++ src/main/scala/ControllerActor.scala
@@ -1,6 +1,11 @@
-/**
-  * Created by markusheilig on 05/11/2016.
-  */
-class ControllerActor {
+import akka.actor.Actor
+
+class ControllerActor extends Actor {
+
+  override def receive = {
+    case msg =>
+      println(s"received message: '$msg'")
+      sender() ! s"hi from scala-chess"
+  }
 
 }
