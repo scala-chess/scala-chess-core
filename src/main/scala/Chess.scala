cdcc7fd1f30e5e71705fea571a124962176d2141
Markus Heilig <markusheilig@gmx.net>
2016-11-05 11:22:23 +0100

    setup ControllerActor

--- src/main/scala/Chess.scala
+++ src/main/scala/Chess.scala
@@ -1,7 +1,15 @@
+import akka.actor.{ActorSystem, Props}
 
 object Chess {
   def main(args: Array[String]): Unit = {
+    setupControllerActor()
     val game = new Game()
     game.run
+  }
+
+  def setupControllerActor(): Unit = {
+    val actorSystem = ActorSystem.create("chess")
+    val chessController = actorSystem.actorOf(Props[ControllerActor], "controller")
+    println(chessController)
   }
 }
